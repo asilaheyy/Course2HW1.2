c95cdb7445b0ab37584cacc9a02ef64b82b78abe
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-10-18 16:12:35 +0300

    methods

--- src/Car.java
+++ src/Car.java
@@ -6,49 +6,80 @@ public class Car {
     int productionYear;
     String productionCountry;
 
-     Car(String brand, String model, int productionYear, String productionCountry, String colour, double engineVolume) {
-         if (brand == null || brand.isEmpty()) {
-             this.brand = "default";
-         } else {
-             this.brand = brand;
-         }
-         if (model == null || model.isEmpty()) {
-             this.model = "default";
-         } else {
-             this.model = model;
-         }
-         if (productionCountry == null || productionCountry.isEmpty()) {
-             this.productionCountry = "default";
-         } else {
-             this.productionCountry = productionCountry;
-         }
-         if (productionYear <= 0) {
-             this.productionYear = 2000;
-         } else {
-             this.productionYear = productionYear;
-         }
-         if (colour == null || colour.isEmpty()) {
-             this.colour = "White";
-         } else {
-             this.colour = colour;
-         }
-         if (engineVolume <= 0) {
-             this.engineVolume = 1.5d;
-         } else {
-             this.engineVolume = engineVolume;
-         }
-     }
-     Car(){
-         this.brand = "default";
-         this.model = "default";
-         this.colour = "White";
-         this.productionCountry = "default";
-         this.engineVolume = 1.5d;
-         this.productionYear = 2000;
-     }
+    Car(String brand, String model, int productionYear, String productionCountry, String colour, double engineVolume) {
+        this.brand = brand;
+        brandNull();
+        this.model = model;
+        modelNull();
+        this.productionCountry = productionCountry;
+        productionCountryNull();
+        this.productionYear = productionYear;
+        productionYearNull();
+        this.colour = colour;
+        colourNull();
+        this.engineVolume = engineVolume;
+        engineVolumeNull();
+    }
+
+    Car() {
+        this.brand = "default";
+        this.model = "default";
+        this.colour = "White";
+        this.productionCountry = "default";
+        this.engineVolume = 1.5d;
+        this.productionYear = 2000;
+    }
 
 
     public void infoAboutCar() {
         System.out.println("Марка " + brand + ", модель " + model + ", год выпуска " + productionYear + ", страна сборки " + productionCountry + ", цвет " + colour + ", объем двигателя - " + engineVolume + " л.");
+    }
+
+    public void brandNull() {
+        if (brand == null || brand.isEmpty()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+    }
+
+    public void modelNull() {
+        if (model == null || model.isEmpty()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+    }
+
+    public void productionCountryNull() {
+        if (productionCountry == null || productionCountry.isEmpty()) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+    }
+
+    public void productionYearNull() {
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+    }
+
+    public void colourNull() {
+        if (colour == null || colour.isEmpty()) {
+            this.colour = "White";
+        } else {
+            this.colour = colour;
+        }
+    }
+
+    public void engineVolumeNull() {
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5d;
+        } else {
+            this.engineVolume = engineVolume;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Car ladaGranta = new Car();
-        Car audiA8 = new Car("Audi","A8 50L TDI quattro", 2020,"Germany","Black", 3.0d);
-        Car bMWZ8 = new Car("BMW", "Z8", 2021, "Germany", "Black", 3.0d);
+        Car audiA8 = new Car("Audi","A8 50L TDI quattro", 2020,null,"Black", 3.0d);
+        Car bMWZ8 = new Car("BMW", "Z8", 2021, "Germany", "Black", 0);
         Car kiaSportage = new Car("Kia","Sportage 4", 2018,"South Korea", "Red",2.4d);
         Car hyundaiAvante = new Car(null, "",2016,"South Korea", "", 0);
 
